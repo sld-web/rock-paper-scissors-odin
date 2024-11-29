c7fc0d46bcbd33e8434850baf0124b9da7d9d507
sld-web <sadeep.web@yahoo.com>
2024-11-29 17:42:11 +0530

    main function

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 console.log("hello")
+
+function getComputerChoice(){
+    const randomNumber = Math.random();
+    if (randomNumber < 0.33){
+        return "Rock"
+    }
+
+    else if (randomNumber < 0.56){
+        return "paper"
+    }
+
+    else {
+        return "scissors"
+    }
+
+}
+
+console.log(getComputerChoice());
+
