55f064a951cdc9b3f7642ffff21249383c461fd4
sld-web <sadeep.web@yahoo.com>
2024-11-29 17:56:08 +0530

    user input function

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 console.log("hello")
 
+
+// main function 
 function getComputerChoice(){
     const randomNumber = Math.random();
     if (randomNumber < 0.33){
@@ -16,5 +18,20 @@ function getComputerChoice(){
 
 }
 
+//user input function
+function getHumanChoice(){
+    // prompt the user for their choice
+    let userChoice = prompt("Enter your choice : rock,paper or scissors");
+    // ensure the user input is valid
+    userChoice = userChoice.toLowerCase();
+    if (userChoice === "rock" || userChoice === "paper" || userChoice === "scissors") {
+        return userChoice;}
+    else{
+        console.log("Invalid choice try again !")
+        return(null);
+    }
+}
+
+console.log(getHumanChoice());
 console.log(getComputerChoice());
 
